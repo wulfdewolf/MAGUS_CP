2b082ca33c9d965850ce6ce11a02f7bd11e1e80f
wulfdewolf <wolf.de.wulf@vub.be>
2022-05-05 17:51:57 +0200

    CP update

--- magus_align/merge/graph_trace/cp.py
+++ magus_align/merge/graph_trace/cp.py
@@ -1,24 +1,25 @@
-
-'''
+"""
 Created on Apr 25, 2022
 
 @author: Wolf De Wulf, Dieter Vandesande
-'''
+"""
 
 from magus_configuration import Configs
 import numpy as np
 from cpmpy import *
-#from magus_tools import external_tools
 
-'''
+# from magus_tools import external_tools
+
+"""
 Resolve clusters into a trace by breaking conflicting clusters apart.
 We use constraint programming (MiniZinc) to search for the solution with the smallest number of clusters broken.
-'''
+"""
+
 
 def CPSearch(graph):
     Configs.log("Finding graph trace with constraint programming..")
-    #external_tools.runMinizincTrace(graph, graph.workingDir, graph.tracePath).run()
-    #graph.readClustersFromCPFile(graph.tracePath)
+    # external_tools.runMinizincTrace(graph, graph.workingDir, graph.tracePath).run()
+    # graph.readClustersFromCPFile(graph.tracePath)
 
     # Create cluster id matrix
     input = [0] * graph.matrixSize
@@ -35,43 +36,79 @@ def CPSearch(graph):
 
     # Transform into numpy array
     input = np.array(input)
-    subalignment_start = np.array([idx + 1 for idx in graph.subsetMatrixIdx] + [len(graph.matrix)])
-    
-    # Setup output 
-    output = intvar(1,input.size,  shape=input.shape, name="output")
-    
+    subalignment_start = np.array(
+        [idx + 1 for idx in graph.subsetMatrixIdx] + [len(graph.matrix)]
+    )
+
+    # Setup output
+    nr_variables = input[input != 0].size
+    output = intvar(1, nr_variables, shape=input.shape, name="output")
+
     # CP using CPMPY
     model = Model()
-    
+
+    # all variables of the same alignment should be ordered strictly increasing.
+    i = 0
     for a in range(subalignment_start.size - 1):
-        for n in range(subalignment_start[a], subalignment_start[a+1]-1):
-            model += (output[n] < output[n+1])
-    
-    
+        for n in range(subalignment_start[a], subalignment_start[a + 1]):
+            if input[n] == 0:
+                continue
+            else:
+                for n1 in range(n + 1, subalignment_start[a + 1] + 1):
+                    if input[n] == 0:
+                        continue
+                    elif n1 == subalignment_start[a + 1]:
+                        break
+                    else:
+                        model += output[i] < output[i + 1]
+                        break
+                i += 1
+
+    # nodes having different cluster ids in the input, should also have different cluster ids in the output
+    i = 0
+    j = 0
     for n1 in range(input.size - 1):
-        for n2 in range(n1+1, input.size):
-            print(str(n1) + " = " + str(input[n1]) +  " , " + str(n2) + " = " + str(input[n2])  )
-            if input[n1] != input[n2]:
-                model += output[n1] != output[n2]
-    
+        if input[n1] == 0:
+            continue
+        else:
+            j = i + 1
+            for n2 in range(n1 + 1, input.size):
+                if input[n2] == 0:
+                    continue
+                elif input[n1] != input[n2]:
+                    model += output[i] != output[j]
+                j += 1
+
+        i += 1
+
     # Check following two possibilities for performance:
-    model.minimize(max([output[subalignment_start[a]-1] for a in range(1, subalignment_start.size)]))
+    model.minimize(
+        max(
+            [
+                output[subalignment_start[a] - 1]
+                for a in range(1, subalignment_start.size)
+            ]
+        )
+    )
     # model.minimize(max(output))
-    
-    print(model)
-    
+
     if model.solve():
-        print(output.value())
+        i = 0
+        for n in range(input.size):
+            if input[n] != 0:
+                input[n] = output[i].value()
+                i += 1
     else:
         print("No solution found")
 
     # Transform output to clusters
-    n_clusters = max(output) 
+    n_clusters = max(input)
     graph.clusters = [[] for _ in range(n_clusters)]
 
     # Loop over matrix
-    for i in range(len(output)):
-        graph.clusters[output[i]-1] = graph.clusters[output[i]-1] + [i]
+    for i in range(len(input)):
+        if input[i] != 0:
+            graph.clusters[input[i] - 1] = graph.clusters[input[i] - 1] + [i]
 
     # Remove singletons
     graph.clusters = [cluster for cluster in graph.clusters if len(cluster) > 1]
