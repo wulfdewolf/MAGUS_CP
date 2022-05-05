93ed6e11f66444d2d8e3b3e6cf7f3896ec7c2d97
wulfdewolf <wolf.de.wulf@vub.be>
2022-05-05 17:54:10 +0200

    mvoed cluster violations

--- magus_align/merge/graph_trace/tracer.py
+++ magus_align/merge/graph_trace/tracer.py
@@ -1,14 +1,17 @@
-'''
+"""
 Created on Jul 17, 2020
 
 @author: Vlad
-'''
+"""
 
 import os
 import time
 
 from magus_configuration import Configs
-from magus_align.merge.graph_cluster.clean_clusters import purgeClusterViolations, purgeDuplicateClusters
+from magus_align.merge.graph_cluster.clean_clusters import (
+    purgeClusterViolations,
+    purgeDuplicateClusters,
+)
 from magus_align.merge.graph_trace.min_clusters import minClustersSearch
 from magus_align.merge.graph_trace.fm import fmAlgorithm
 from magus_align.merge.graph_trace.mwt_search import mwtGreedySearch, mwtSearch
@@ -17,32 +20,33 @@ from magus_align.merge.graph_trace.rg_fast_search import rgFastSearch
 from magus_align.merge.graph_trace.naive import naiveClustering
 from magus_align.merge.graph_trace.cp import CPSearch
 
-'''
+"""
 Graph clusters must be refined into a "trace", a constrained clustering that corresponds to a valid MSA.
 There are a variety of ways to do this, "minclusters" is usually the most dependable option.
 "mwtgreedy" or "rgfast" might be used if there are scalability issues.
 Some of these options don't require an existing clustering, and can work on the raw graph.
-'''
+"""
+
 
 def findTrace(graph):
-    time1 = time.time() 
-    
+    time1 = time.time()
+
     if os.path.exists(graph.tracePath):
         Configs.log("Found existing trace file {}".format(graph.tracePath))
         graph.readClustersFromFile(graph.tracePath)
-        
+
     else:
         purgeDuplicateClusters(graph)
+        purgeClusterViolations(graph)
 
         if Configs.graphTraceMethod == "cp":
             CPSearch(graph)
         else:
-            purgeClusterViolations(graph)
-        
+
             if Configs.graphTraceMethod == "minclusters":
-                minClustersSearch(graph)       
-            elif Configs.graphTraceMethod == "fm":            
-                fmAlgorithm(graph)        
+                minClustersSearch(graph)
+            elif Configs.graphTraceMethod == "fm":
+                fmAlgorithm(graph)
             elif Configs.graphTraceMethod == "mwtgreedy":
                 mwtGreedySearch(graph)
             elif Configs.graphTraceMethod == "mwtsearch":
@@ -53,14 +57,13 @@ def findTrace(graph):
                 rgFastSearch(graph)
             elif Configs.graphTraceMethod == "naive":
                 naiveClustering(graph)
-        
+
         graph.writeClustersToFile(graph.tracePath)
-    
-    
+
     time2 = time.time()
-    Configs.log("Found alignment graph trace in {} sec..".format(time2-time1))
-    Configs.log("Found a trace with {} clusters and a total cost of {}".format(len(graph.clusters), graph.computeClusteringCost(graph.clusters)))
-    
-    
-    
-    
+    Configs.log("Found alignment graph trace in {} sec..".format(time2 - time1))
+    Configs.log(
+        "Found a trace with {} clusters and a total cost of {}".format(
+            len(graph.clusters), graph.computeClusteringCost(graph.clusters)
+        )
+    )
